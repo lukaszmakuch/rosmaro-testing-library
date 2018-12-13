75d4ad2faaeb054e82b4177084fbac28332e9957
Łukasz Makuch <zopsesen@gmail.com>
2018-12-13 19:31:31 +0100

    Started.

---
+++ __tests__/module.js
@@ -0,0 +1,52 @@
+import testFlow from './../src';
+
+const model = ({state = 0, action}) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {state: state + 1};
+    case 'DECREMENT':
+      return {state: state - 1};
+    case 'READ':
+      return {state, result: state};
+    default:
+      return {state};
+  }
+};
+
+test('testing a stateful flow', () => {
+
+  let verifyArgs = [];
+  const verify = arg => verifyArgs.push(verifyArgs);
+
+  testFlow([
+
+    {
+      action: {type: 'READ'},
+      verifyResult: ({result}) => {
+        verify({afterReadingForTheFirstTime: result});
+      },
+    },
+
+    {
+      action: {type: 'INCREMENT'},
+      verifyResult: ({result}) => {
+        verify({afterIncrementingForTheFirstTime: result});
+      },
+    },
+
+    {
+      action: {type: 'READ'},
+      verifyResult: ({result}) => {
+        verify({afterReadingForTheSecondTime: result});
+      },
+    },
+
+  ]);
+
+  expect(verifyArgs).toEqual([
+    {afterReadingForTheFirstTime: 0},
+    {afterIncrementingForTheFirstTime: undefined},
+    {afterReadingForTheSecondTime: 1},
+  ]);
+
+});
